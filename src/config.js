00e52145011db2d06adec084820d39be8d230eae
Arn786 <abdurnaveed786@gmail.com>
2018-10-12 19:26:57 -0700

    test

--- src/config.js
+++ src/config.js
@@ -1,7 +1,5 @@
 export default {
-  //clientId: process.env.REACT_APP_CLIENT_ID,
-  clientId: '85391446082-j51j9i28ma9ict8irdm29402aaau32aj.apps.googleusercontent.com',
+  clientId: process.env.REACT_APP_CLIENT_ID,
   scope: "https://www.googleapis.com/auth/spreadsheets",
-  spreadsheetId:'1sPrH_5ek67aBZ9I4sIdx1QkaYDdo1x9dMAgU9WKKo8k'
-  //spreadsheetId: process.env.REACT_APP_SPREADSHEET_ID
+  spreadsheetId: process.env.REACT_APP_SPREADSHEET_ID
 };
