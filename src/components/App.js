7d9bc5c16fbe20ae4ee2e754a5dd2ff3ca7fc7a6
Abdur-Rehman Naveed <abdurnaveed786@gmail.com>
2019-10-24 12:07:16 -0700

    Changed 6479 website link

--- src/components/App.js
+++ src/components/App.js
@@ -19,7 +19,7 @@ class App extends Component {
         <footer className="App-footer">
           <p>
             Made by{' '}
-            <a href="https://sites.google.com/view/cdsrobotics">AZTECH 6479</a>
+            <a href="team6479.org">AZTECH 6479</a>
           </p>
         </footer>
       </div>
